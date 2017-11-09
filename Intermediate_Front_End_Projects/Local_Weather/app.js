5617ad7d909216f37fafdd9b18c9ffb807a8a42b
cwadsworth33 <cwadsrun33@gmail.com>
2017-11-08 20:17:02 -0500

    changes backgrounds based on weather conditions

--- Intermediate_Front_End_Projects/Local_Weather/app.js
+++ Intermediate_Front_End_Projects/Local_Weather/app.js
@@ -9,13 +9,16 @@ $(document).ready(function() {
     position.coords.latitude+"&lon="+position.coords.longitude, function(json){
         //$("#data").html(JSON.stringify(json));
         $("#location").html(json.name + ", "+json.sys.country);
-        $("#temp").html(json.main.temp + " C");
-        $("#clouds").html(json.weather[0].main);
+        $("#temp").html(json.main.temp + " &degC");
+        $("#clouds").html(json.weather[0].main + " ("+json.weather[0].description+")");
         console.log(json);
-        //var x = document.createElement("IMG");
-        //x.src = json.weather[0].icon;
-        //var imagePar = document.getElementById("icon");
-        //imagePar.appendChild(x);
+        var x = document.createElement("IMG");
+        x.src = json.weather[0].icon;
+        var imagePar = document.getElementById("icon");
+        imagePar.appendChild(x);
+
+        //setTheme(json.weather[0].main);
+        setTheme("Night");
       });
   });
 }
@@ -28,12 +31,38 @@ function convert(){
     temp = temp * 9/5 + 32;
     isCelsius = false;
     temp = Math.round( temp * 10) / 10;
-    x.innerHTML = "" + temp + " F";
+    x.innerHTML = "" + temp + " &degF";
   }
   else{
     temp = (temp - 32) * 5/9;
     isCelsius = true;
     temp = Math.round( temp * 100) / 100;
-    x.innerHTML = "" + temp + " C";
+    x.innerHTML = "" + temp + " &degC";
+  }
+}
+
+function setTheme(weather){
+  if(weather == "Clouds"){
+    $("div").css("background-color","#C67A43");
+    $("h1").css("color","#0B5293");
+    $("button").css("background-color","#5A4D44");
+    $('body').css('background-image', 'url(' + "clouds.jpeg" + ')');
+    $("#quote").html("&quotThere are no rules of architecture for a castle in the clouds.&quot<br>-Gilbert K. Chesterton");
+  }
+  else if(weather == "Rain"){
+    $("div").css("background-color","#C67A43");
+    $('body').css('background-image', 'url(' + "rain.jpeg" + ')');
+  }
+  else if(weather == "Sun"){
+    $("div").css("background-color","#C67A43");
+    $('body').css('background-image', 'url(' + "spring.jpeg" + ')');
+  }
+  else if(weather == "Clear"){
+    $("div").css("background-color","#C67A43");
+    $('body').css('background-image', 'url(' + "clear.jpeg" + ')');
+  }
+  else if(weather == "Night"){
+    $("div").css("background-color","#C67A43");
+    $('body').css('background-image', 'url(' + "night-trees-stars.jpg" + ')');
   }
 }
