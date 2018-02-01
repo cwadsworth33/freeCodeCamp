97a49b7f84d40aa0534b95d9f4d7855844d2376e
cwadsworth33 <cwadsrun33@gmail.com>
2018-02-01 15:13:18 -0500

    added navbar and social media icons

--- Basic_Front_End_Projects/Portfolio/script.js
+++ Basic_Front_End_Projects/Portfolio/script.js
@@ -21,3 +21,21 @@ $("#sec6scroll").click(function() {
         scrollTop: $("#section1").offset().top
     }, 1000);
 });
+
+$("#about").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#section2").offset().top
+    }, 1000);
+});
+
+$("#projects").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#section4").offset().top
+    }, 1400);
+});
+
+$("#contact").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#section6").offset().top
+    }, 1500);
+});
