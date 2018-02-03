78a961bf110596b5d88f38a819d5c6615d9a3c6e
cwadsworth33 <cwadsrun33@gmail.com>
2018-02-02 23:45:19 -0500

    Changed formatting to use a sticky footer

--- Basic_Front_End_Projects/Portfolio/script.js
+++ Basic_Front_End_Projects/Portfolio/script.js
@@ -1,41 +1,41 @@
 $("#sec1scroll").click(function() {
     $('html, body').animate({
         scrollTop: $("#section2").offset().top
-    }, 1000);
+    }, 1200);
 });
 
 $("#sec2scroll").click(function() {
     $('html, body').animate({
         scrollTop: $("#section4").offset().top
-    }, 1000);
+    }, 1200);
 });
 
 $("#sec3scroll").click(function() {
     $('html, body').animate({
         scrollTop: $("#section6").offset().top
-    }, 1000);
+    }, 1200);
 });
 
 $("#sec6scroll").click(function() {
     $('html, body').animate({
         scrollTop: $("#section1").offset().top
-    }, 1000);
+    }, 1700);
 });
 
 $("#about").click(function() {
     $('html, body').animate({
         scrollTop: $("#section2").offset().top
-    }, 1000);
+    }, 1200);
 });
 
 $("#projects").click(function() {
     $('html, body').animate({
         scrollTop: $("#section4").offset().top
-    }, 1400);
+    }, 1700);
 });
 
 $("#contact").click(function() {
     $('html, body').animate({
         scrollTop: $("#section6").offset().top
-    }, 1500);
+    }, 1700);
 });
